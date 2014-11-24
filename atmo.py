88ff7bab93a008220bc7d5b4691d2d7cddc31ebb
Joachim Moeyens <joachim@Joachims-MacBook-Pro.local>
2014-11-24 00:28:26 -0800

    Added wavelength interpolation.
    Updated scripts to interpolate the wavelength range to the appropriate
    grid in atmo.py. Still in testing phase.

--- atmo.py
+++ atmo.py
@@ -3,21 +3,21 @@ import pylab
 import os
 import copy
 
-class atmo:   
+class atmo:
     def __init__(self):
         # List of strings containing component names
         self.components = ['H2O','O2','O3','Rayleigh','Aerosol']
         # List of strings containing components names to be used when plotting
         self.componentsPlot = ['$H_2O$','$O_2$','$O_3$','Rayleigh','Aerosol']
-        # List of parameters (H2O,O2,O3,Rayleigh,Aerosol,Alpha). 
+        # List of parameters (H2O,O2,O3,Rayleigh,Aerosol,Alpha).
         self.parameters = [1.0,1.0,1.0,1.0,1.0,1.7]
         # List of parameters used for plotting
         self.parametersPlot = ['$t_{H_2O}$','$t_{O_2}$','$t_{O_3}$','$t_{Rayleigh}$','$t_{Aerosol}$','$alpha$']
         self.componentsColor = ['blue','green','red','purple','cyan']
         self.wavelength = None
         self.airmasses = None
-        self.atmoTrans = None  
-        
+        self.atmoTrans = None
+    
     def readModtranFiles(self, modtranDir='.', modtranRoot='Pachon_MODTRAN',modtranSuffix='.7sc'):
         files = os.listdir('.')
         modtranFiles = []
@@ -28,8 +28,8 @@ class atmo:
         
         if len(modtranFiles) > 0:
             print "Found " + str(len(modtranFiles)) + " MODTRAN files:"
-        
-        self.wavelength = numpy.arange(250,1100.5,0.5,dtype='float')
+    
+        self.wavelength = numpy.arange(300,1100.5,0.5,dtype='float')
         self.atmoTemplates = {}
         self.atmoTrans = {}
         self.airmasses = []
@@ -42,7 +42,7 @@ class atmo:
             
             for comp in self.components:
                 transTemp[comp] = []
-                    
+            
             for lineNum,line in enumerate(fin):
                 if lineNum < 4:
                     if lineNum == 1:
@@ -65,6 +65,7 @@ class atmo:
             templates = {}
             for comp in self.components:
                 trans[comp] = numpy.array(transTemp[comp],dtype='float')
+                trans[comp] = numpy.interp(self.wavelength, wavelenTemp, transTemp[comp], left=0.0, right=0.0)
             self.airmasses.append(self.airmassToString(airmass))
             self.atmoTrans[airmass] = copy.deepcopy(trans)
         if self.atmoTrans != None:
@@ -85,30 +86,30 @@ class atmo:
         
         w=self.wavelength
         fig, ax = pylab.subplots(1,1)
-        fig.set_size_inches(12,6) 
-
+        fig.set_size_inches(12,6)
+        
         ax.plot(w,self.genAtmo(w,P,X,aerosolNormCoeff),color=genAtmoColor,label=self.labelGen(P));
         ax.set_xlabel("Wavelength, $\lambda$ (nm)")
         ax.set_ylabel("Transmission")
         ax.set_title("$S^{atm}(\lambda)$ and $S^{atm}_{std}(\lambda)$");
-        ax.legend(loc='lower right',shadow=False) 
+        ax.legend(loc='lower right',shadow=False)
         ax.set_xlim(wavelengthRange[0],wavelengthRange[1]);
         
         if includeStdAtmo == True:
             stdAtmoParams = [1.0,1.0,1.0,1.0,1.0,1.7]
             ax.plot(w,self.genAtmo(w,stdAtmoParams,X=stdAtmoAirmass,aerosolNormCoeff=stdAerosolNormCoeff),
-                        label=self.labelGen(stdAtmoParams),alpha=stdAtmoColorAlpha,color=stdAtmoColor);
-                
-        ax.legend(loc='lower right',shadow=False) 
-        return
+                    label=self.labelGen(stdAtmoParams),alpha=stdAtmoColorAlpha,color=stdAtmoColor);
         
+        ax.legend(loc='lower right',shadow=False)
+        return
+
     def aerosol(self,w,X,alpha=1.7,aerosolNormCoeff=0.1):
         return numpy.e**(-aerosolNormCoeff*X*(550.0/w)*alpha)
     
     def airmassToString(self,airmass):
         X = float(airmass)
         return "%.3f" % (X)
-        
+    
     def labelGen(self,P):
         label = []
         for paramNum,param in enumerate(P):
